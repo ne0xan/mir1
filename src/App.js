1cbd606ecb4e3a11ef99c97f94b0380f123f320e
александр риттер <aleksandrritter@MacBook-aleksandr.local>
2024-06-04 21:44:16 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+    const onclose = () => {
+
+    }
+  return (
+    <div className="App">
+        <button>
+            Закрыть
+        </button>
+    </div>
+  );
+}
+
+export default App;
